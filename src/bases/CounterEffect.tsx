949565c2027135b39a8b332a9cd0cbad33eb7c2e
giselars <gisela-rs@outlook.com>
2023-01-17 23:17:48 +0100

    :art: improve structure and add react components

---
+++ src/bases/CounterEffect.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react"
+
+const MAXIMUN_COUNT = 10;
+
+export const CounterEffect = () => {
+
+    const [counter, setCounter] = useState(5);
+    
+    const handleClick = () => {
+        setCounter( prev => Math.min( prev + 1, MAXIMUN_COUNT ));
+    }
+
+    useEffect(() => {
+
+        if ( counter < 10 ) return;
+
+        console.log('%cSe llegó al valor máximo','color:red;background-color:black;')
+
+    }, [counter])
+    
+
+  return (
+    <>
+        <h1>CounterEffect: { counter }</h1>
+        
+        <button onClick={ handleClick }>+ 1</button>
+    </>
+  )
+}
